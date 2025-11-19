ae45ba16c55757fce99b258e407f5ebc3fa46fc7
Nicolas Pinto <nicolaspinto2805@gmail.com>
2025-11-19 10:37:41 -0300

    Add main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./modules/main/pages/MainPage";
 import LoginPage from "./modules/auth/pages/LoginPage";
 import AdminPage from "./modules/admin/pages/AdminPage";
 import RegisterPage from "./modules/auth/pages/RegisterPage";
@@ -8,13 +9,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Ruta de login */}
-        <Route path="/" element={<LoginPage />} />
 
-        {/* Ruta de registro */}
+        <Route path="/" element={<MainPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+
         <Route path="/register" element={<RegisterPage />} />
 
-        {/* Rutas de administrador */}
         <Route path="/admin/*" element={<AdminPage />} />
 
         <Route path="/cart/*" element={<CartPage />} />
